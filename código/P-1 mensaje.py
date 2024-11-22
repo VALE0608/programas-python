859bc43a72d09a61bc11cde420d3832e0d67d751
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-21 21:49:25 -0600

    Create P-1 mensaje.py

---
+++ código/P-1 mensaje.py
@@ -0,0 +1,6 @@
+# Programa que imprime un mensaje
+# Fecha: 21/11/2024
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Espero que tengas un gran día 😊")
