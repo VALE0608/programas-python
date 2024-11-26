446995beea8713f90e168f1d0b849d0d595944b4
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-25 20:52:48 -0600

    Create P-3 num. de dias de la semana, nombre de una figura de 3 lados y ¿a cuántos metros equivalen 250cm?.py

---
+++ código/BLOQUE 1/P-3 num. de dias de la semana, nombre de una figura de 3 lados y ¿a cuántos metros equivalen 250cm?.py
@@ -0,0 +1,7 @@
+# Programa 3: Imprime el num. de dias de la semana, nombre de una figura de 3 lados y ¿a cuántos metros equivalen 250cm?
+# fecha: 2024/10/14
+# elaborado por: Valeria Jaqueline Rodarte García
+
+print(7)#numero de dias de la semnana (int)
+print("triángulo")#nombre de una figura de 3 lados (str)
+print(2.5)#¿a cuántos metros equivalen 250cm? (float)
