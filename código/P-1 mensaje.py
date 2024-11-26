02161e6acdb9bf06c264380676276c501bbd2f72
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-25 20:08:01 -0600

    Update P-1 mensaje.py

--- código/P-1 mensaje.py
+++ código/P-1 mensaje.py
@@ -1,5 +1,6 @@
-# Programa 1: programa que imprime un mensaje de texto en pantalla
-# fecha de elaboración: 2024/10/14
-# elaborado por: Valeria Jaqueline Rodarte García
+# Programa 1: Programa que solicita tu nombre y después te immprime un saludo personalizado
+# Fecha de elaboración: 2024/10/14
+# Elaborado por: Valeria Jaqueline Rodarte García
+
 nombre = input("¿Cómo te llamas?")
 print("Hola,", nombre,"!")
