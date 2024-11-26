3c7b328bb475f61c0df60474bd82eb7e86286e87
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-25 20:39:25 -0600

    Update and rename P-2 tipos de datos.py to P-2 Programa que solicita el nombre al usuario y lo imprime con un mensaje personalizado.py

---
+++ código/P-2 Programa que solicita el nombre al usuario y lo imprime con un mensaje personalizado.py
@@ -0,0 +1,6 @@
+#programa 2: Programa que solicita el nombre al usuario y lo imprime con un mensaje personalizado
+# fecha de elaboración: 2024/10/14
+# elaborado por: Valeria Jaqueline Rodarte García
+
+nombre = input("¿Cúal es tu nombre?")
+print("Hola,", nombre, "!:), Buen día")
