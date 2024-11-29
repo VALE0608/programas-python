d3e67e7ad534dc06b6405c022520590365926dd2
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-28 21:18:32 -0600

    Create P-5  programa que suma, resta, multiplica y divide.py

---
+++ código/BLOQUE 1/P-5  programa que suma, resta, multiplica y divide.py
@@ -0,0 +1,16 @@
+# Programa 5: Programa que suma, resta, multiplica y divide
+# Fecha: 2024/10/15
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+num_1 = int(input("Ingresa el primer valor:"))
+num_2 = int(input("Ingresa el segundo valor:"))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+multiplicación = num_1 * num_2
+división = num_1 / num_2
+
+print("El valor de la suma es:" + str(suma))
+print("El valor de la resta es:" + str(resta))
+print("El valor de la multiplicación es:" + str(multiplicación))
+print("El valor de la división es:" + str(división))
