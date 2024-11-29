0894d30bacc0d6f5f0511bd061fd5cc68c42d459
VALERIA RODARTE <vjrg.06@gmail.com>
2024-11-28 21:01:57 -0600

    Create P-4  programa el funcionamiento de suma, resta, multiplicacion y suma de cadenas.py

---
+++ código/BLOQUE 1/P-4  programa el funcionamiento de suma, resta, multiplicacion y suma de cadenas.py
@@ -0,0 +1,7 @@
+# Programa 4: Programar el funcionamiento del sig. código
+# fecha: 2024/10/14
+# elaborado por: Valeria Jaqueline Rodarte García
+print(1 + 2)#suma
+print(1 - 2)#resta
+print(1 * 2)#multiplicacion
+print("Sumando cadenas" + " " + "está bien")#suma 
