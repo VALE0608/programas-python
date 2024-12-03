3bf89231ed4aa178b9539a24e8ec10885d40322b
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-02 22:04:52 -0600

    Create P-12 Programa para elaborar las compuertas lógicas AND, OR y NOT.py

---
+++ código/BLOQUE 1/P-12 Programa para elaborar las compuertas lógicas AND, OR y NOT.py
@@ -0,0 +1,15 @@
+# Programa p/probar las compuertas lógicas AND, OR y NOT
+# Fecha: 2024/10/18
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print("\nCompuerta OR")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print("\nCompuerta AND")
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
