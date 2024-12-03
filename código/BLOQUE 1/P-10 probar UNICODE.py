f275ff98568a5b8452adc8ff0ad747fc3a6947fe
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-02 21:19:47 -0600

    Create P-10 probar UNICODE.py

---
+++ código/BLOQUE 1/P-10 probar UNICODE.py
@@ -0,0 +1,9 @@
+#probar UNICODE
+# Fecha: 2024/10/17
+# Elaborado por: Valeria Jaqueline Rodarte García
+# Ejemplos de emojis con Unicode
+print("\U0001F600")  #  Carita feliz
+print("\U0001F609")  #  Carita guiñando un ojo
+print("\U0001F62D")  #  Carita llorando
+print("\U0001F44D")  #  Pulgar hacia arriba
+print("\U0001F64C")  #  Manos alzadas
