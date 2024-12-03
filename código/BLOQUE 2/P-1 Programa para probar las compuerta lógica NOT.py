53e90f3839b39b46e898ce0e01ad99bf442a7dc6
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-02 22:45:53 -0600

    Create P-1 Programa para probar las compuerta lógica NOT.py

---
+++ código/BLOQUE 2/P-1 Programa para probar las compuerta lógica NOT.py
@@ -0,0 +1,7 @@
+# Programa para probar las compuerta lógica NOT
+# Fecha: 2024/10/22
+# Elaborado por: Valeria Jaqueline Rodarte García
+print("\nCompuerta NOT")
+
+print(not True) # Imprime False
+print(not False) # Imprime True
