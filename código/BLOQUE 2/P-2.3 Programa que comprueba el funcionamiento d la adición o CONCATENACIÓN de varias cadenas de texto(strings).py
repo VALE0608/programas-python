4123ec39a6361ec6c4aa1adcaa0ee3e7a5f491b2
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-03 07:31:48 -0600

    Create P-2.3 Programa que comprueba el funcionamiento d la adición o CONCATENACIÓN de varias cadenas de texto(strings).py

---
+++ código/BLOQUE 2/P-2.3 Programa que comprueba el funcionamiento d la adición o CONCATENACIÓN de varias cadenas de texto(strings).py
@@ -0,0 +1,4 @@
+# Programa que comprueba el funcionamiento d la adición o CONCATENACIÓN de varias cadenas de texto(strings)
+# Fecha: 2024/10/22
+# Elaborado por: Valeria Jaqueline Rodarte García
+print("Mi" + "nombre" + "es" + "Edinguer")
