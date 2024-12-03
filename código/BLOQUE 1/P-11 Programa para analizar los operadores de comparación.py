9f3b238d7b85fb4dbe04a3b70a6c07719c051a41
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-02 21:38:21 -0600

    Create P-11 Programa para analizar los operadores de comparación.py

---
+++ código/BLOQUE 1/P-11 Programa para analizar los operadores de comparación.py
@@ -0,0 +1,34 @@
+# Programa p/analizar los operadores de comparación: ==, !=, >=, <=, >, <
+# Fecha: 2024/10/18
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print("Operador IGUAL (==)")
+print(5 == 7) # FALSE
+print(10 == 7) # FALSE
+print(5.0 == 5) # TRUE
+
+print("\nOperador DIFERENTE O NO IGUAL (!=)")
+print(5 != 5) # FALSE
+print(10 != 7) # TRUE
+print(5.0 != 5) # FALSE
+
+print("\nOperador MAYOR QUE (>)")
+print(5 > 5) # FALSE
+print(10 > 7) # TRUE 
+print(5.0 > 5) # FALSE 
+
+print("\nOperador MENOR QUE (<)")
+print(5 < 10) # TRUE
+print(10 < 5) # FALSE 
+
+print("\nOperador MENOR O IGUAL QUE (<=)")
+print(5 <= 5) # TRUE
+print(10 <= 7) # FALSE
+print(3 <= 5) # TRUE
+
+print("\nOperador MAYOR O IGUAL QUE (>=)")
+print(5 >= 5) # TRUE
+print(10 >= 7) # TRUE
+print(3 >= 5) # FALSE
+
+print("\u263A" * 10)
