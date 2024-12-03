39720c17b4e89165c7f0e1bf594d98e677965675
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-03 07:18:08 -0600

    Create P-2.2  Programa que si dos personas van a la fiesta, entonces NO hay fiesta.py

---
+++ código/BLOQUE 2/P-2.2  Programa que si dos personas van a la fiesta, entonces NO hay fiesta.py
@@ -0,0 +1,8 @@
+# Programa que si Chorichis y Choto van a la fiesta, entonces NO hay fiesta
+# Fecha: 2024/10/22
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print(not(False or False)) # SI hay fiesta
+print(not(False or True)) # NO hay fiesta
+print(not(True or False)) # NO hay fiesta
+print(not(True or True)) # NO hay fiesta
