d200a05ef95bca6c707939398421e3023936430a
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:34:37 -0600

    Create P_4 Programa que imprima el nombre de tus 10 personajes favoritos de( ).py

---
+++ docs/BLOQUE 4/P_4 Programa que imprima el nombre de tus 10 personajes favoritos de( ).py
@@ -0,0 +1,9 @@
+# Programa 4: Programa que imprima el nombre de tus 10 personajes favoritos de...
+# Fecha: 2024/11/4
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+personajes_favoritos = ["Pantera Negra", "Spider Man", "Natasha Romanoff", "Iron Man", "Capitán América", "Thor",
+"Wonder Woman", "Peter Kevinsky", "Lara Jean", "Flash"]
+
+for personaje in personajes_favoritos:
+    print(personaje)
