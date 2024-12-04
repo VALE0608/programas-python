0ed7f06d48d060784fd317a59b9f97e5e7240344
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:39:06 -0600

    Create P_10 Programa que demuestre el funcionamiento de lo métodos upper() y lower().py

---
+++ docs/BLOQUE 4/P_10 Programa que demuestre el funcionamiento de lo métodos upper() y lower().py
@@ -0,0 +1,12 @@
+# Programa 10: Programa que demuestre el funcionamiento de lo métodos upper() y lower()
+# Fecha: 2024/11/8
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+# Definimos una cadena de texto
+cadena = "Python es un lenguaje de programacón"
+print(cadena)
+cadena_mayusculas = cadena.upper()
+print(cadena_mayusculas)
+
+cadena_minusculas = cadena.lower()
+print(cadena_minusculas)
