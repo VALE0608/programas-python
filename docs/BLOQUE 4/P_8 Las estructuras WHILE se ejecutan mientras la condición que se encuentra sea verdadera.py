10895b5f0a17a91e25ffb2296c0cf9303dcaddc0
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:38:03 -0600

    Create P_8 Las estructuras WHILE se ejecutan mientras la condición que se encuentra sea verdadera.py

---
+++ docs/BLOQUE 4/P_8 Las estructuras WHILE se ejecutan mientras la condición que se encuentra sea verdadera.py
@@ -0,0 +1,22 @@
+# Programa 8: Las estructuras WHILE se ejecutan mientras la condición que se encuentra sea verdadera 
+# Fecha: 2024/11/7
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+# Ejemplo 1: imprimir los números del 1 al 10
+print("Números del 1 al 10")
+i = 1 # Inicialización de la variable control
+while i <= 10:# Condición de parada
+    print(i)
+    i += 1 # Equivalente a i = i + 1
+
+# Sintaxis 
+# while <condición>:
+#   <cuerpo del while>
+
+# Ejemplo 2
+# Imprimir los números del 10 al 1
+print("\nNúmeros del 10 al 1")
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
