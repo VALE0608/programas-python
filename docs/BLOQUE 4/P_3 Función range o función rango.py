7b59d254e512fdb2fec8ea031ec601d88df28098
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:33:52 -0600

    Create P_3 Función range o función rango.py

---
+++ docs/BLOQUE 4/P_3 Función range o función rango.py
@@ -0,0 +1,22 @@
+# Programa 3: Función range o función rango
+# Fecha: 2024/11/4
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+x = range(10)
+for num in x:
+    print(num)
+
+print("\nImprime los valores del 5 al 15")
+x1 = range(5, 16)
+for num in x1:
+    print(num)
+
+print("\nImprime los pares del 10 al 20")
+x2 = range(10, 21, 2)
+for num in x2:
+    print(num)
+
+print("\nImprime los impares del 10 al 20")
+x3 = range(11, 21, 2)
+for num in x3:
+    print(num)
