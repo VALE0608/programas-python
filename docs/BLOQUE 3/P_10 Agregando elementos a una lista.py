44a1b409a18638aa2d9fb7f60c3c03da405ca953
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:27:37 -0600

    Create P_10 Agregando elementos a una lista.py

---
+++ docs/BLOQUE 3/P_10 Agregando elementos a una lista.py
@@ -0,0 +1,9 @@
+# Programa 10: Agregando elementos a una lista
+# Fecha: 2024/10/31
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+colores = ["rojo", "azul"]
+print(colores)
+colores.append("verde")
+print(colores)
+#print(colores + "Amarillo") error
