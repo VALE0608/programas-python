7e529b7dbf2e2c03bb42bb2285d0634ad7762765
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:21:38 -0600

    Create P_8 Indexing y Subscripting.py

---
+++ docs/BLOQUE 3/P_8 Indexing y Subscripting.py
@@ -0,0 +1,16 @@
+# Programa 7: Tipo de datos lista y tipos de datos de los elementos
+# Fecha: 2024/10/31
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print("Indexing")
+numeros = [10, 20, 30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+print(numeros[-1])
+
+print("\nSubscripting")
+nombres = ["Chorchis", "Choto", "Emiliano", "Pepe el toro"]
+print(nombres[1:])
+print(nombres[1:5])
+print(nombres[-2:])
