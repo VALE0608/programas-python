b78a19c1dc5c28274c0250b9aa67e061a64c4487
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-03 19:56:06 -0600

    Create P_5 Programa que ejecuta la comparación, membership e indexing.py

---
+++ código/BLOQUE 2/P_5 Programa que ejecuta la comparación, membership e indexing.py
@@ -0,0 +1,23 @@
+# Programa que ejecuta la comparación, membership e indexing
+# Fecha: 2024/10/22
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+#Comparación
+print("\nComparación")
+print("perro" == "perro")#True
+print("perro" != "gato")#True
+print("Aguascalientes" < "Zacatecas")#True
+print("Aire" >= "Agua")#True
+
+#Membership
+print("\nMembership")
+print("house" in "house")#True
+print("bien" in "bienvenidos")#True
+print("y" not in "ejes")#True
+print("je" not in "ejes")#False
+
+#Indexing
+print("\nIndexing")
+mi_nombre = "Choto Chorchis"
+print(mi_nombre[3])
+print(mi_nombre[12])
