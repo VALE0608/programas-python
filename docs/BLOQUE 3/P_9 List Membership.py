12002f5f0dc1c070e86e013730df7d805e8247f5
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:27:05 -0600

    Create P_9 List Membership.py

---
+++ docs/BLOQUE 3/P_9 List Membership.py
@@ -0,0 +1,10 @@
+# Programa 9: List Membership
+# Fecha: 2024/10/31
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+nombres = ["Choto", "Emilio", "Luis"]
+
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("José" not in nombres) #True
