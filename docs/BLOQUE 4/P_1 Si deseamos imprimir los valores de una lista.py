576ec8670e10063399f72bacf3b01a369485cf7b
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:32:29 -0600

    Create P_1 Si deseamos imprimir los valores de una lista.py

---
+++ docs/BLOQUE 4/P_1 Si deseamos imprimir los valores de una lista.py
@@ -0,0 +1,15 @@
+# Programa 1: Si deseamos imprimir los valores de una lista, por ejemplo:
+# Fecha: 2024/11/4
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+lista_num = [10, 30, 40, 20, 35, 80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+
+# Haciendo lo mismo pero con un ciclo "for"
+for i in lista_num:
+    print(i)
