052e7169a509e4876a00049f3ec3eeffdd47c29e
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-03 20:22:54 -0600

    Create P_7 Programa que solicite la edad e indique si se puede entrar a un bar.py

---
+++ código/BLOQUE 2/P_7 Programa que solicite la edad e indique si se puede entrar a un bar.py
@@ -0,0 +1,10 @@
+# Programa que solicite la edad e indique si se puede entrar a un bar
+# Fecha: 2024/10/24
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+edad = int(input("Ingresa tu edad:"))
+if edad>=18:
+    print("Puede ingresar al bar")
+else: 
+    print("Vete a tu casa")
+print("Fin del programa")
