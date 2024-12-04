264261488eb06b3da6ccb657938b35f92d85aa9c
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:19:23 -0600

    Create P_6 Listas.py

---
+++ docs/BLOQUE 3/P_6 Listas.py
@@ -0,0 +1,15 @@
+# Programa 6: Listas
+# Fecha: 2024/10/29
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print("Lista de enteros ") 
+print([40, 20, 35])
+
+print("\nLista de cadenas de texto") 
+print("Manzana", "piña", "Naranja")
+
+print("\nLista de booleanos")
+print([False, True, True])
+
+print("\nLista vacia") 
+print([])
