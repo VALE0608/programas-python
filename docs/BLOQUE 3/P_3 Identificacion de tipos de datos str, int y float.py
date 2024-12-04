e6d9ce99dff9a8c3b460d0ef4e5d069f1fc9ce28
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:17:02 -0600

    Create P_3 Identificacion de tipos de datos str, int y float.py

---
+++ docs/BLOQUE 3/P_3 Identificacion de tipos de datos str, int y float.py
@@ -0,0 +1,12 @@
+# Programa 3 Identificacion de tipos de datos str, int y float
+# Fecha: 2024/10/28
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+variable = input("Ingresa tu edad: ")
+print(type(variable))
+
+variable = int(variable)     # Convierto la variable a tipo entero
+print(type(variable))
+
+variable = float(variable)   # Convierto la variable a tipo flotante
+print(type(variable))
