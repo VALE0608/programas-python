5d5b3b2b4467b27e04a32e97e9d1f85823e1978c
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:33:17 -0600

    Create P_2Si deseamos imprimir los valores de una lista.py

---
+++ docs/BLOQUE 4/P_2Si deseamos imprimir los valores de una lista.py
@@ -0,0 +1,13 @@
+# Programa 2: Si deseamos imprimir los valores de una lista, por ejemplo:
+# Fecha: 2024/11/4
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+nombres = ["Luis", "Chuy", "Mauricio"]
+
+for nombre in nombres:
+    print("El nombre es:", nombre)
+
+frutas = ["Manzana", "Piña", "Plátano"]
+
+for fruta in frutas:
+    print("La fruta es:", fruta)
