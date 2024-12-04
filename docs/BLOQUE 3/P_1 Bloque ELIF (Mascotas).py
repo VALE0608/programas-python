df84df6ca07175aaaf71e12dbc5c543e0748d34c
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:14:58 -0600

    Create P_1 Bloque ELIF (Mascotas).py

---
+++ docs/BLOQUE 3/P_1 Bloque ELIF (Mascotas).py
@@ -0,0 +1,14 @@
+# Programa 1 Bloque ELIF (Mascotas)
+# Fecha: 2024/10/28
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+nombre = input("Ingresa el tipo de mascota: ")
+if "perro" in nombre:
+    print("Es un perro")
+
+elif "gato" in nombre: 
+    print("Es un gato")
+else:
+     print("Mascota desconocida")
+
+print("¡Gracias por usar nuestro programa!")
