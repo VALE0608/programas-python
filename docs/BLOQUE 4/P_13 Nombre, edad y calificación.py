a7c54f83e5b9fe0495cf19a3e98a1d98bb3d6be9
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:41:19 -0600

    Create P_13 Nombre, edad y calificación.py

---
+++ docs/BLOQUE 4/P_13 Nombre, edad y calificación.py
@@ -0,0 +1,11 @@
+# Programa 13: Nombre, edad y calificación
+# Fecha: 2024/11/12
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+nombre = "Edinguer"
+edad = 27
+calificación: 93.8
+print("Mi nombre es: " + nombre + "mi edad es: " + str(edad) + "mi calificación es: " + calificación)
+
+print(f"Nombre{nombre}, edad:{edad}, promedio:{calificación}")
+print("Nombre: %5, edad %d, promedio %2f" %(nombre, edad, calificación))
