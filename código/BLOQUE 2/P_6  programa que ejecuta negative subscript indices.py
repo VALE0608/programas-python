3aa7ad9fd02fa1457c6e558b2beed35539b72a70
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-03 20:11:03 -0600

    Create P_6  programa que ejecuta negative subscript indices.py

---
+++ código/BLOQUE 2/P_6  programa que ejecuta negative subscript indices.py
@@ -0,0 +1,10 @@
+# programa que ejecuta negative subscript indices
+# Fecha: 2024/10/24
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+print("\nNegative Subscript Indices")
+word = "hamster"
+print(word[-1])
+print(word[1:-1])
+print(word[-3:])
+print(word[:3])
