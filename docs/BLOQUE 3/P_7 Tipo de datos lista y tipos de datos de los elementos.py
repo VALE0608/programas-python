6d7e3a512277bfc6ff14fbfa83a1f6e98c0fa991
VALERIA RODARTE <vjrg.06@gmail.com>
2024-12-04 16:20:01 -0600

    Create P_7 Tipo de datos lista y tipos de datos de los elementos.py

---
+++ docs/BLOQUE 3/P_7 Tipo de datos lista y tipos de datos de los elementos.py
@@ -0,0 +1,21 @@
+# Programa 7: Tipo de datos lista y tipos de datos de los elementos
+# Fecha: 2024/10/30
+# Elaborado por: Valeria Jaqueline Rodarte García
+
+cadenas = ["Juan", "Pedro", "Chuy"]
+print(cadenas) # Muestra la lista completa
+print(cadenas[1]) # Imprime "Pedro"
+print(type(cadenas)) #Imprime el tipo "list"
+print(type(cadenas[1])) #Imprime el tipo "str"
+
+enteros = ["10", "15", "20"]
+print(enteros) # Muestra la lista completa
+print(enteros[0]) # Imprime "10"
+print(type(enteros)) #Imprime el tipo "list"
+print(type(enteros[1])) #Imprime el tipo "int"
+
+booleanos = ["True", "False", "True"]
+print(booleanos) # Muestra la lista completa
+print(booleanos[0]) # Imprime "True"
+print(type(booleanos)) #Imprime el tipo "list"
+print(type(booleanos[1])) #Imprime el tipo "bool"
